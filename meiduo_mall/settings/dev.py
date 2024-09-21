a24a923e2d30075c16a840f9ea59e141dbb63dc8
路飞 <xoutm@foxmail.com>
2024-09-21 19:20:42 +0800

    修改配置

--- meiduo_mall/settings/dev.py
+++ meiduo_mall/settings/dev.py
@@ -151,7 +151,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 # 配置日志
 LOGGING = {
     'version': 1,
